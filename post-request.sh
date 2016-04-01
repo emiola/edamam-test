f9c1066e55ce5080591cc836d95bd878320e8618
Antonio Forgione <emiola@gmail.com>
2016-04-01 01:25:27 -0500

    Use env variable to pass app_id and app_key.

--- post-request.sh
+++ post-request.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
-curl -d @"$1" -H "Content-Type: application/json" "https://api.edamam.com/api/nutrition-details?app_id=af305f50&app_key=d7ed44530190e11d8f11d02aac9646f4"
+#echo "$1" "$APP_ID" "$APP_KEY"
+curl -d @"$1" -H "Content-Type: application/json" "https://api.edamam.com/api/nutrition-details?app_id=$APP_ID&app_key=$APP_KEY"
 
